58697ffac2cd655d9268425caa3bf189818dfd75
Durss <francois.dursus@gmail.com>
2022-01-12 20:09:12 +0100

    Fixing mark as read feature

--- src/components/newusers/NewUsers.vue
+++ src/components/newusers/NewUsers.vue
@@ -4,22 +4,30 @@
 			<h1>Greet them <span class="count">({{messages.length}})</span></h1>
 			<Button :icon="require('@/assets/icons/delete.svg')" class="clearBt" data-tooltip="Clear all messages" @click="clearAll()" />
 		</div>
-		<ChatMessage v-for="(m,index) in messages"
-			ref="message"
-			class="message"
-			:key="m.id"
-			:messageData="m"
-			:deleteOverlay="index<=overIndex"
-			@mouseover="onMouseOver(index)"
-			@mouseout="onMouseOut(index)"
-			@click="deleteMessage(m, index)" />
+		
+		<transition-group name="fade" appear
+			@leave="leave"
+			tag="div"
+		>
+			<ChatMessage
+				v-for="(m,index) in messages"
+				:key="m.tags.id"
+				class="message"
+				:messageData="m"
+				:data-index="index"
+				ref="message"
+				:deleteOverlay="(streakMode && index<=overIndex) || index == overIndex"
+				@mouseover="onMouseOver($event, index)"
+				@mouseout="onMouseOut()"
+				@click="deleteMessage(m, index)" />
+		</transition-group>
 	</div>
 </template>
 
 <script lang="ts">
-import ChatMessage, { ChatMessageData } from '@/components/messages/ChatMessage.vue';
-import IRCClient from '@/utils/IRCClient';
-import IRCEvent from '@/utils/IRCEvent';
+import ChatMessage from '@/components/messages/ChatMessage.vue';
+import store from '@/store';
+import { IRCEventDataList } from '@/utils/IRCEvent';
 import Utils from '@/utils/Utils';
 import gsap from 'gsap/all';
 import { Options, Vue } from 'vue-class-component';
@@ -35,56 +43,99 @@ import Button from '../Button.vue';
 export default class NewUsers extends Vue {
 
 	public overIndex:number = -1;
+	public streakMode:boolean = true;
+	public indexOffset:number = 0;
 
-	private messages:IRCEvent[] = [];
-	private uidsDone:{[key:string]:boolean} = {};
-	private messageHandler!:(e:unknown)=>void;
+	private keyboardEventHandler!:(e:KeyboardEvent) => void;
 
-	public async mounted():Promise<void> {
-		this.messageHandler = (e:unknown) => this.onMessage(e as IRCEvent);
-		IRCClient.instance.addEventListener(IRCEvent.MESSAGE, this.messageHandler);
+	public get messages():IRCEventDataList.Message[] {
+		return (store.state.chatMessages as IRCEventDataList.Message[]).filter(m => m.firstMessage);
 	}
 
-	public async beforeUnmount():Promise<void> {
-		IRCClient.instance.removeEventListener(IRCEvent.MESSAGE, this.messageHandler);
+	public mounted():void {
+		this.keyboardEventHandler = (e:KeyboardEvent) => {
+			if(e.key != "Control" && e.key != "Shift") return;
+
+			if(e.type == "keyup" && !this.streakMode) {
+				this.streakMode = true;
+			}else if(e.type == "keydown") {
+				this.streakMode = false;
+			}
+		};
+		document.addEventListener("keydown", this.keyboardEventHandler);
+		document.addEventListener("keyup", this.keyboardEventHandler);
 	}
 
-	private async onMessage(e:IRCEvent):Promise<void> {
-		if(this.uidsDone[e.tags['user-id'] as string] === true) return;
-
-		this.messages.push(e);
-		this.uidsDone[e.tags['user-id'] as string] = true;
+	public beforeUnmount():void {
+		document.removeEventListener("keydown", this.keyboardEventHandler);
+		document.removeEventListener("keyup", this.keyboardEventHandler);
 	}
 
-	public deleteMessage(m:ChatMessageData, index:number):void {
-		const instances = this.$refs.message as Vue[];
-		const divs = instances.splice(0, index+1).map(i => i.$el as HTMLDivElement);
-		gsap.to(divs, {
-			duration:0.2,
-			height:0,
-			margin:0,
-			padding:0,
-			opacity:0,
-			ease:'sine.in',
-			stagger:0.05,
-			onComplete:()=>{
-				this.messages.splice(0, index+1);
-				this.overIndex = -1;
+	public deleteMessage(m:IRCEventDataList.Message, index:number):void {
+		let el = (this.$refs["message"] as Vue[])[index] as ChatMessage;
+		this.indexOffset = parseInt(el.$el.dataset.index as string)
+		
+		if(!this.streakMode) {
+			m.firstMessage = false;
+		}else{
+			this.overIndex = -1;
+			let messages = this.messages;
+			let index = messages.findIndex(v => v.tags.id == m.tags.id);
+			for (let i = 0; i < index+1; i++) {
+				messages[i].firstMessage = false;
+			}
+		}
+	}
+
+	public clearAll():void {
+		let messages = this.messages;
+		Utils.confirm("Clear all", "You are about to clear all messages.", null, "Confirm", "Cancel").then(() => {
+			for (let i = 0; i < messages.length; i++) {
+				if(messages[i].firstMessage) {
+					messages[i].firstMessage = false;
+				}
 			}
 		});
 	}
 
-	public clearAll():void {
-		Utils.confirm("Clear all", "You are about to clear all messages.", null, "Confirm", "Cancel").then(() => {
-			this.messages = [];
-		});
-	}
-
-	public onMouseOver(index:number):void {
+	public onMouseOver(e:MouseEvent, index:number):void {
 		this.overIndex = index;
 	}
-	public onMouseOut(index:number):void {
+	public onMouseOut():void {
 		this.overIndex = -1;
+	}
+
+	public enter(el:HTMLElement, done:()=>void):void {
+		gsap.from(el, {
+			duration:0.2,
+			height:0,
+			// scaleY:0,
+			margin:0,
+			padding:0,
+			ease:'sine.in',
+			clearProps:'all',
+			onComplete:()=>{
+				done();
+			}
+		});
+	}
+	public leave(el:HTMLElement, done:()=>void):void {
+		console.log(this.indexOffset);
+		let delay = (parseInt(el.dataset.index as string)-this.indexOffset) * 0.07;
+		gsap.to(el, {
+			duration:0.3,
+			height:0,
+			// scaleY:0,
+			margin:0,
+			padding:0,
+			fontSize:0,
+			delay,
+			ease:'sine.in',
+			clearProps:'all',
+			onComplete:()=>{
+				done();
+			}
+		});
 	}
 
 }
@@ -134,6 +185,7 @@ export default class NewUsers extends Vue {
 	.message {
 		cursor: pointer;
 		overflow: hidden;
+		transition: all 0.2s;
 
 		&:nth-child(odd) {
 			background-color: fade(#ffffff, 2.5%);
@@ -141,6 +193,24 @@ export default class NewUsers extends Vue {
 
 		&:hover {
 			background-color: fade(#ffffff, 5%);
+		}
+		/* Enter and leave animations can use different */
+		/* durations and timing functions.              */
+		.fade-enter-active {
+			transition: all 0.2s;
+		}
+
+		.fade-leave-active {
+			transition: all 0.2s;
+		}
+
+		.fade-enter-from,
+		.fade-leave-to {
+			height: 0;
+			margin: 0;
+			padding: 0;
+			opacity: 0;
+			transform: scaleY(0);
 		}
 	}
 }
