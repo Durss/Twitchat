a3fd582933a4d40693ec121af5594c84a4825145
Durss <francois.dursus@gmail.com>
2022-05-26 01:16:44 +0200

    Adding warning if nobody is allowed

--- src/components/params/contents/obs/OBSPermissions.vue
+++ src/components/params/contents/obs/OBSPermissions.vue
@@ -5,6 +5,7 @@
 		<ParamItem :paramData="param_subs" class="row" @change="$emit('update:subs', param_subs.value); $emit('update')"/>
 		<ParamItem :paramData="param_all" class="row" @change="$emit('update:all', param_all.value); $emit('update')"/>
 		<ParamItem :paramData="param_users" class="row" @change="$emit('update:users', param_users.value); $emit('update')"/>
+		<div v-if="noSelection" class="noSelection">Nobody is allowed by the current selection</div>
 	</div>
 </template>
 
@@ -53,6 +54,14 @@ export default class OBSPermissions extends Vue {
 	public param_all:ParameterData		= { type:"toggle", value:false, label:"Everyone", icon:"user_purple.svg" };
 	public param_users:ParameterData	= { type:"text", value:"", label:"Specific users", longText:true, placeholder:"user1, user2, user3, ..." };
 
+	public get noSelection():boolean {
+		return this.param_mods.value === false
+		&& this.param_vips.value === false
+		&& this.param_subs.value === false
+		&& this.param_all.value === false
+		&& this.param_users.value === "";
+	}
+
 	public mounted():void {
 		this.param_mods.value	= this.mods;
 		this.param_vips.value	= this.vips;
@@ -78,6 +87,13 @@ export default class OBSPermissions extends Vue {
 	
 	&>:not(:first-child) {
 		margin-top: .25em;
+	}
+
+	.noSelection {
+		padding: .25em;
+		border-radius: .25em;
+		color: @mainColor_light;
+		background-color: fade(@mainColor_alert, 100%);
 	}
 }
 </style>
