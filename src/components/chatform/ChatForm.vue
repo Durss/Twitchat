9650a0a899e1951b4d3cedc39768f447c3fd3435
Durss <francois.dursus@gmail.com>
2022-05-25 04:06:29 +0200

    Better emotes insert logic

--- src/components/chatform/ChatForm.vue
+++ src/components/chatform/ChatForm.vue
@@ -492,11 +492,11 @@ export default class ChatForm extends Vue {
 			}
 			this.autoCompleteSearch = "";
 		}else{
-			const prefix = /\s/gi.test(localMessage.charAt(carretPos-1))? "" : " ";
-			const suffix = /\s/gi.test(localMessage.charAt(carretPos-1+item.length))? "" : " ";
+			const prefix = carretPos == 0 || /\s/gi.test(localMessage.charAt(carretPos))? "" : " ";
+			const suffix = carretPos == localMessage.length || /\s/gi.test(localMessage.charAt(carretPos+1))? "" : " ";
 			const code = prefix + item + suffix;
-			localMessage = localMessage.substring(0, carretPos) + code + localMessage.substring(carretPos);
-			carretPos += code.length;
+			localMessage = localMessage.substring(0, carretPos+1) + code + localMessage.substring(carretPos+1);
+			carretPos += code.length+1;
 		}
 		
 		if(/\{.*?\}/gi.test(item)) {
