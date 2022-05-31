670ce5fb980f13948d5c2f40ef4a8227f6966189
Durss <francois.dursus@gmail.com>
2022-05-31 03:14:39 +0200

    updating wheel's design

--- src/components/overlays/OverlaysRaffleWheel.vue
+++ src/components/overlays/OverlaysRaffleWheel.vue
@@ -1,15 +1,17 @@
 <template>
 	<div class="overlaysrafflewheel">
 		<InfiniteList class="list" ref="list"
-		v-if="itemList.length > 0"
+		v-show="listDisplayed"
+		v-if="listEnabled"
 		:data="itemList"
 		:width="'100%'"
 		:height="listHeight"
 		:itemSize="itemSize"
 		:debug="false"
 		:scrollOffset="scrollOffset"
+		:overscanCount="0"
 		v-slot="{ item }">
-			<div class="item" :style="getStyles(item)">{{ item.label }}</div>
+			<div class="item">{{ item.label }}</div>
 		</InfiniteList>
 	</div>
 </template>
@@ -32,29 +34,44 @@ export default class OverlaysRaffleWheel extends Vue {
 	public itemSize:number = 50;
 	public scrollOffset:number = 0;
 	public listHeight:number = 100;
+	public listEnabled:boolean = false;
+	public listDisplayed:boolean = false;
 
+	private inc:number = .1;
+	private vy:number = 0;
+	private vy_eased:number = 0;
+	private rafID:number = 0;
+	private animStep:number = 0;
 	private resizeHandler!:()=>void;
-
-	/**
-	 * The background color is there to better understand
-	 * the scrolling direction
-	 * 
-	 * @param data
-	 */
-	public getStyles(data:ListItem):{[key:string]:string} {
-		return {
-			// backgroundColor: data.color,
-		};
-	}
+	private resizeDebounce!:number;
+	private prevBiggestItem!:HTMLDivElement;
 
 	public async mounted():Promise<void> {
-		this.resizeHandler = ()=> {
-			this.listHeight = document.body.clientHeight;
-			this.itemSize = this.listHeight / 7;
+		this.resizeHandler = async ()=> {
+			if(this.listHeight != document.body.clientHeight) {
+				this.listEnabled = false;
+				this.listDisplayed = false;
+				this.rafID = Math.random();
+				clearTimeout(this.resizeDebounce);
+				this.resizeDebounce = setTimeout(()=>this.populate(), 50);
+			}
 		}
 		this.resizeHandler();
-		document.body.addEventListener("resize", this.resizeHandler);
-		
+		window.addEventListener("resize", this.resizeHandler);
+	}
+
+	public beforeUnmount():void {
+		this.rafID = 0;
+		window.removeEventListener("resize", this.resizeHandler);
+	}
+
+	private async populate():Promise<void> {
+		await this.$nextTick();
+		this.listHeight = document.body.clientHeight;
+		this.itemSize = this.listHeight / 15;
+		this.scrollOffset = 0;
+		this.listEnabled = true;
+
 		let list = [];
 		let len = 100;
 		let baseColor = Utils.getLessVars().mainColor_dark as string;
@@ -67,10 +84,10 @@ export default class OverlaysRaffleWheel extends Vue {
 				value:i, 
 				// alpha:(i%20)/20 * .05,
 				// alpha:Math.sin(i/(len*.5) * Math.PI * 2)*.025+.025,
-				color:baseColor.replace("1", alpha.toString()),
+				color:baseColor.replace("1)", alpha.toString()+")"),
 			});
 		}
-		
+
 		// const originalItemCount = list.length;
 		list = list.concat(list);
 		do {
@@ -83,14 +100,68 @@ export default class OverlaysRaffleWheel extends Vue {
 		//The put a setTimeout() on the mount method so we can't
 		//just wait a frame with $nextTick().
 		setTimeout(()=>{
-			this.scrollOffset = originalItemCount * this.itemSize;
-			const endScroll = this.scrollOffset + originalItemCount * this.itemSize;
-			gsap.to(this, {duration:10, delay:1, scrollOffset:endScroll, ease:"back.out"});
-		},100)
+			this.scrollOffset = 5 * this.itemSize;
+			//Give it a bit of time to actually scroll
+			setTimeout(()=> {
+				this.renderFrame(this.rafID);
+				this.vy = -5;
+				this.vy_eased = this.vy;
+				//Show the list only then, once it had time to render to avoid glitches
+				this.listDisplayed = true;
+			}, 100)
+		},100);
 	}
 
-	public beforeDestroy():void {
-		document.body.removeEventListener("resize", this.resizeHandler);
+	private renderFrame(id:number):void {
+		if(id != this.rafID) return;
+		requestAnimationFrame(()=>this.renderFrame(id));
+
+		const items = this.$el.querySelectorAll(".vue3-infinite-list");
+		// const list = this.$refs.list as Vue;
+		// const sublistHolder = (list.$el as HTMLDivElement).getElementsByTagName("div")[0] as HTMLDivElement;
+		// let offsetY = sublistHolder.getBoundingClientRect().top;
+		// sublistHolder.style.perspectiveOrigin = "center "+(-offsetY + this.listHeight/2)+"px";
+		const h = this.listHeight;
+		let biggestItem!:HTMLDivElement;
+		let biggestItemScale = 0;
+		for (let i = 0; i < items.length; i++) {
+			const item = items[i] as HTMLDivElement;
+			const rect = item.getBoundingClientRect();
+			const cy = rect.top + rect.height/2;
+			const ratio = ((h/2 - cy) / (h/2));
+			// const angle = ratio * 45;
+			// const pz = -Math.abs(ratio) * 200;
+			const py = -ratio * 50;
+			const scale = (Math.abs(Math.cos(ratio))) * .75 + .25;
+			item.style.opacity = ((1-Math.abs(ratio))*.2 + .8).toString();
+			item.style.transform = "scale("+scale+") translateY("+py+"px)"; //translateZ("+pz+"px) rotateX("+angle+"deg)
+			if(scale > biggestItemScale) {
+				biggestItem = item;
+				biggestItemScale = scale;
+			}
+		}
+		// console.log(offsetY);
+		if(this.prevBiggestItem
+		&& this.prevBiggestItem != biggestItem
+		&& this.prevBiggestItem.classList.contains("selected")) {
+			this.prevBiggestItem.classList.remove("selected");
+		}
+		biggestItem.classList.add("selected");
+		this.prevBiggestItem = biggestItem;
+
+		this.scrollOffset += this.vy_eased;
+		if(this.vy < 10 && this.animStep == 0) {
+			this.vy += this.inc;
+		}else{
+			if(this.animStep < 2) {
+				this.animStep = 1;
+				this.vy *= .995;
+			}else if(this.animStep == 2){
+				this.vy *= .95;
+				this.inc *= .95;
+			}
+		}
+		this.vy_eased += (this.vy - this.vy_eased) * .1;
 	}
 }
 
@@ -104,21 +175,46 @@ interface ListItem {
 <style scoped lang="less">
 .overlaysrafflewheel{
 	height: 100%;
+	@borderWidth: 3px;
 	.list {
 		height: 100%;
-		overflow: hidden !important;
+		max-width: 700px;
+		// overflow: hidden !important;
 		:deep(.vue3-infinite-list) {
-			padding-bottom: 10px;
+			transform-origin: left center;
+			.item {
+				border-top-right-radius: 2em;
+				border-bottom-right-radius: 2em;
+				@numberOfItems: 15;
+				font-size: ~"calc(100vh / (@{numberOfItems}*2))";
+				display: flex;
+				justify-content: center;
+				align-items: center;
+				color: @mainColor_dark;
+				height: 100%;
+				background-color: @mainColor_light;
+				transition: all .2s;
+				will-change: transform;//Avoid text jittering
+				border: @borderWidth solid @mainColor_dark;
+				border-left: none;
+				margin-right: 15%;
+			}
+			&.selected {
+				z-index: 1;
+				width: 100%;
+				&>.item {
+					margin-right: 0;
+					background-color: @mainColor_highlight_extralight;
+					@scaleAdd: 50%;
+					height: calc(100% + @scaleAdd);
+					transform: translateY(calc(-@scaleAdd/4 - @borderWidth));
+				}
+			}
 		}
-		.item {
-			display: flex;
-			justify-content: center;
-			align-items: center;
-			color: @mainColor_light;
-			height: 100%;
-			background-color: @mainColor_dark;
-			box-shadow: 0 2px 2px rgba(0,0,0,.5);
-		}
+		// &>:deep(div) {
+			// perspective: 800px;
+			// perspective-origin: center top;
+		// }
 	}
 }
 </style>
