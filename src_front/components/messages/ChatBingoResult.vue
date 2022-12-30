2b22fcb4fc5eea29d10fb9e515daf411b2444f98
Durss <francois.dursus@gmail.com>
2022-12-30 01:38:53 +0100

    Making bingo notif localizable

--- src_front/components/messages/ChatBingoResult.vue
+++ src_front/components/messages/ChatBingoResult.vue
@@ -3,12 +3,19 @@
 	@click="$emit('onRead', messageData, $event)">
 		<span class="time" v-if="$store('params').appearance.displayTime.value">{{time}}</span>
 		<img src="@/assets/icons/bingo.svg" alt="icon" class="icon">
-		<div>
-			<a class="userlink" v-if="messageData.bingoData.winners![0]" @click.stop="openUserCard(messageData.bingoData.winners![0])">{{messageData.bingoData.winners![0].displayName}}</a> won the bingo with answer
-			<img class="answer emote" :src="messageData.bingoData.emoteValue?.twitch?.image.hd" v-if="messageData.bingoData.guessEmote">
-			<strong class="answer" v-else-if="messageData.bingoData.guessNumber">{{messageData.bingoData.numberValue}}</strong>
-			<strong class="answer" v-else>{{messageData.bingoData.customValue}}</strong>
-		</div>
+
+		<i18n-t scope="global" tag="div" keypath="chat.bingo.title">
+			<template #WINNER>
+				<a class="userlink"
+				v-if="messageData.bingoData.winners![0]"
+				@click.stop="openUserCard(messageData.bingoData.winners![0])">{{messageData.bingoData.winners![0].displayName}}</a>
+			</template>
+			<template #ANSWER>
+				<img class="answer emote" :src="messageData.bingoData.emoteValue?.twitch?.image.hd" v-if="messageData.bingoData.guessEmote">
+				<strong class="answer" v-else-if="messageData.bingoData.guessNumber">{{messageData.bingoData.numberValue}}</strong>
+				<strong class="answer" v-else>{{messageData.bingoData.customValue}}</strong>	
+			</template>
+		</i18n-t>
 	</div>
 </template>
 
