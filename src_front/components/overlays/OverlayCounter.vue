693a324014438904ce38907760d5d1b476e1cdc0
Durss <francois.dursus@gmail.com>
2023-01-25 08:07:08 +0100

    Allowing value transition on embed mode Removing decimals

--- src_front/components/overlays/OverlayCounter.vue
+++ src_front/components/overlays/OverlayCounter.vue
@@ -3,7 +3,7 @@
 
 		<div class="counter" id="holder" v-if="counter.min === false && counter.max === false">
 			<span class="name" id="name">{{ counter.name }}</span>
-			<span class="spacer" id="spacer">|</span>
+			<span class="spacer" id="spacer"></span>
 			<span class="value" id="value">{{ value }}</span>
 		</div>
 
@@ -24,10 +24,9 @@
 import TwitchatEvent from '@/events/TwitchatEvent';
 import type { TwitchatDataTypes } from '@/types/TwitchatDataTypes';
 import PublicAPI from '@/utils/PublicAPI';
-import Utils from '@/utils/Utils';
 import gsap from 'gsap';
-import type { StyleValue } from 'vue';
-import { Options, Vue } from 'vue-class-component';
+import { watch, type StyleValue } from 'vue';
+import { Options, Vue, } from 'vue-class-component';
 
 @Options({
 	props:{
@@ -44,7 +43,9 @@ export default class OverlayCounter extends Vue {
 	public embed!:boolean;
 	public staticCounterData!:TwitchatDataTypes.CounterData;
 
-	public progrressMode:boolean = false;;
+	public localValue:number = 0;
+	public fillWidth:number = 0;
+	public progrressMode:boolean = false;
 	public counter:TwitchatDataTypes.CounterData|null = null;
 
 	private id:string = "";
@@ -52,21 +53,13 @@ export default class OverlayCounter extends Vue {
 	private counterUpdateHandler!:(e:TwitchatEvent) => void;
 
 	public get value():number {
-		return parseFloat(this.counter!.value.toFixed(2));
+		return parseFloat(this.localValue.toFixed(0));
 	}
 
 	public get progressStyles():StyleValue {
 		let res:StyleValue = {};
 
-		let min = Math.min(this.counter!.min || 0);
-		let max = Math.min(this.counter!.max || 0);
-		let minPrev = min;
-		min = Math.min(min, max);
-		max = Math.max(minPrev, max);
-		const percent = (this.counter!.value - min)/(max - min);
-		console.log(percent, min, max);
-
-		res.width = (percent*100)+"%";
+		res.width = this.fillWidth+"%";
 		
 		return res;
 	}
@@ -75,6 +68,7 @@ export default class OverlayCounter extends Vue {
 		if(this.embed !== false) {
 			this.counter = this.staticCounterData;
 			this.setCounterData(this.counter);
+			this.onValueUpdate();
 		}else{
 			this.id = this.$route.query.cid as string ?? "";
 			if(this.id) {
@@ -85,6 +79,10 @@ export default class OverlayCounter extends Vue {
 				PublicAPI.instance.broadcast(TwitchatEvent.COUNTER_GET, {cid:this.id});
 			}
 		}
+
+		watch(()=>this.counter?.value, ()=> {
+			this.onValueUpdate();
+		});
 	}
 
 	public beforeUnmount(): void {
@@ -95,13 +93,28 @@ export default class OverlayCounter extends Vue {
 		if(e.data) {
 			const c = ((e.data as unknown) as {counter:TwitchatDataTypes.CounterData}).counter;
 			if(c.id != this.id) return;
-			if(this.counter) {
-				const duration = Math.min(.25, Math.max(1, Math.abs(this.counter.value - c.value) * .1));
-				gsap.to(this.counter, {duration, value:c.value});
-				await Utils.promisedTimeout(10000);
-			}
+			// if(this.counter) {
+			// 	const duration = Math.min(.25, Math.max(1, Math.abs(this.counter.value - c.value) * .1));
+			// 	gsap.to(this.counter, {duration, value:c.value});
+			// 	await Utils.promisedTimeout(duration * 1000);
+			// }
 			this.setCounterData(c);
 		}
+	}
+
+	private onValueUpdate():void {
+		if(!this.counter) return;
+		// const duration = Math.min(5, Math.max(.25, Math.abs(this.counter.value - this.localValue) * .025));
+		const duration = 1;
+		gsap.to(this, {duration, localValue:this.counter?.value, ease:"sine.inOut"});
+
+		let min = Math.min(this.counter.min || 0);
+		let max = Math.min(this.counter.max || 0);
+		let minPrev = min;
+		min = Math.min(min, max);
+		max = Math.max(minPrev, max);
+		const percent = (this.counter.value - min)/(max - min);
+		this.fillWidth = (percent*100);
 	}
 
 	private setCounterData(data:TwitchatDataTypes.CounterData):void {
@@ -130,9 +143,13 @@ export default class OverlayCounter extends Vue {
 			text-shadow: 0px 0px .05em black;
 			&.value {
 				font-weight: bold;
+				font-variant-numeric: ordinal;
 			}
 			&.spacer {
 				margin: 0 .25em;
+				&::before {
+					content:"|";
+				}
 			}
 		}
 	}
@@ -151,6 +168,7 @@ export default class OverlayCounter extends Vue {
 			font-weight: bold;
 			z-index: 1;
 			position: absolute;
+			left: 0;
 			top: 50%;
 			transform: translate(0, calc(-50% - .3em));
 			width: 100%;
@@ -165,7 +183,7 @@ export default class OverlayCounter extends Vue {
 			left: 0;
 			background: @mainColor_normal_extralight;
 			height: 100%;
-			transition: width .5s;
+			transition: width 1s ease-in-out;
 		}
 	
 		.goal {
@@ -175,17 +193,19 @@ export default class OverlayCounter extends Vue {
 			display: flex;
 			flex-direction: row;
 			justify-content: space-between;
+			align-items: flex-end;
 			bottom: 0;
 			left: 0;
 
 			span {
 				text-shadow: 0px 0px .075em black;
 				border-radius: .25em;
-				padding: .25em;
+				padding: .15em .25em;
 
 				&.value {
 					font-weight: bold;
 					font-size: 1.25em;
+					font-variant-numeric: ordinal;
 				}
 			}
 		}
