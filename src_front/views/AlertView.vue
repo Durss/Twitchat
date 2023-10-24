be4faf81fd87304fcaefa8f63204ad497623f2e9
Durss <francois.dursus@gmail.com>
2023-10-24 15:32:52 +0200

    Finishing forgotten WIP

--- src_front/views/AlertView.vue
+++ src_front/views/AlertView.vue
@@ -1,8 +1,11 @@
 <template>
-	<div class="alert" v-if="message && message.length > 0" @click="close()">
+	<div class="alertview" v-if="message && message.length > 0" @click="close()">
 		<CloseButton v-if="!locked" />
-		<p v-html="message" class="label"></p>
-		<p>{{ $t("global.alert_support") }}<Button :href="discordUrl" type="link" target="_blank" icon="discord" light alert>{{ $t("global.join_discordBt") }}</Button></p>
+		<div v-html="message" class="label"></div>
+		<div v-if="$store('main').alertData.showContact" class="contact">
+			<span>{{ $t("global.alert_support") }}</span>
+			<Button :href="discordUrl" type="link" target="_blank" icon="discord" light alert>{{ $t("global.join_discordBt") }}</Button>
+		</div>
 	</div>
 </template>
 
@@ -21,7 +24,6 @@ import { Component, Vue } from 'vue-facing-decorator';
 	},
 })
 export default class AlertView extends Vue {
-
 	public message = "";
 	public timeout!:number;
 	public locked:boolean = false;
@@ -51,13 +53,18 @@ export default class AlertView extends Vue {
 			if(mess.critical) {
 				this.locked = true;
 			}else{
-				this.timeout = setTimeout(()=> this.close(), this.message.length*80 +2000);
+				const addTime = mess.showContact? 6000 : 2000;
+				this.timeout = setTimeout(()=> this.close(), this.message.length*80 + addTime);
 			}
 		}else if(this.message) {
 			gsap.to(this.$el, {duration:.3, height:0, paddingTop:0, paddingBottom:0, ease:"back.in", onComplete:()=> {
 				this.message = "";
 			}});
 		}
+	}
+
+	public beforeUnmount():void {
+		clearTimeout(this.timeout);
 	}
 
 	public close():void {
@@ -70,26 +77,39 @@ export default class AlertView extends Vue {
 </script>
 
 <style lang="less" scoped>
-
-.alert {
+.alertview {
 	background-color: var(--color-alert);
 	color: var(--color-light);
 	padding: 20px 0;
 	height: auto;
 	width: 100%;
-	position: fixed;
 	overflow: hidden;
 	z-index: 101;
 	position: fixed;
 	top: 0;
 	left: 0;
 	cursor: pointer;
+	gap: 1em;
+	display: flex;
+	flex-direction: column;
 
 	.label {
 		max-width: 600px;
-		margin: auto;
+		margin: 0 auto;
 		text-align: center;
 		line-height: 1.3em;
+	}
+
+	.contact {
+		align-self: center;
+		padding: .5em;
+		gap: .5em;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		.bevel();
+		background-color: var(--color-alert-dark);
+		border-radius: var(--border-radius);
 	}
 }
 </style>
