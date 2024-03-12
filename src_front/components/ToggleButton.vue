731af297be5f2772e1070295f9e7e0eb2f2bdf85
Durss <francois.dursus@gmail.com>
2024-03-12 13:23:06 +0100

    fix: using native checkbox as source of events

--- src_front/components/ToggleButton.vue
+++ src_front/components/ToggleButton.vue
@@ -148,8 +148,10 @@ export default toNative(ToggleButton);
 		max-height: @size;
 		top: 0;
 		left: 0;
+		width: 100%;
+		height: 100%;
 		opacity: 0.001;
-		z-index: -1;
+		z-index: 1;
 		cursor: pointer;
 	}
 
