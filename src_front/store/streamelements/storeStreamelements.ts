72ddb7e87acfe88e174b2e50acbd71373b9ed3e4
Durss <francois.dursus@gmail.com>
2024-03-20 13:08:31 +0100

    fix: validating CSRF token

--- src_front/store/streamelements/storeStreamelements.ts
+++ src_front/store/streamelements/storeStreamelements.ts
@@ -119,6 +119,8 @@ export const storeStreamelements = defineStore('streamelements', {
 		
 		async getAccessToken():Promise<boolean> {
 			try {
+				const csrfResult = await ApiHelper.call("auth/CSRFToken", "POST", {token:this.authResult.csrf});
+				if(!csrfResult.json.success) return false;
 				const result = await ApiHelper.call("streamelements/auth", "POST", this.authResult, false)
 				if(result.json.success) {
 					this.accessToken = result.json.accessToken!;
