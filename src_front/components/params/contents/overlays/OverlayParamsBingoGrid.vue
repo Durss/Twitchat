1eadacbe790969a84796fc8594cae439e8e07ebe
Durss <francois.dursus@gmail.com>
2024-05-22 02:53:20 +0200

    feat: ading "new flags" and removing fake youtube tuto on bingo grid

--- src_front/components/params/contents/overlays/OverlayParamsBingoGrid.vue
+++ src_front/components/params/contents/overlays/OverlayParamsBingoGrid.vue
@@ -3,11 +3,11 @@
 
 		<div class="header">{{ $t("overlay.bingo_grid.head") }}</div>
 
-		<a href="https://www.youtube.com/watch?v=0S9SgAi8IOI" target="_blank" class="youtubeTutorialBt">
+		<!-- <a href="https://www.youtube.com/watch?v=0S9SgAi8IOI" target="_blank" class="youtubeTutorialBt">
 			<Icon name="youtube" theme="light" />
 			<span>{{ $t('overlay.youtube_demo_tt') }}</span>
 			<Icon name="newtab" theme="light" />
-		</a>
+		</a> -->
 
 		<BingoGridForm embedMode></BingoGridForm>
 	</div>
