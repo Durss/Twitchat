be5e22fb6d4fdb6a3f8295b3cd5cae4e099e0232
Durss <francois.dursus@gmail.com>
2024-09-12 03:02:43 +0200

    chore: fixing version upgrade sequence

--- scripts/versionSuffix.mjs
+++ scripts/versionSuffix.mjs
@@ -1,18 +1,36 @@
 import * as fs from "fs";
 import * as path from "path";
 import {fileURLToPath} from 'url';
+import { execSync } from "child_process";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+const args = process.argv.slice(2);
+
+//Version upgrade type major, minor or patch
+const upgradeType = args[0] || "minor";
+
 // Load the package.json file
 const packageJsonPath = path.join(__dirname, '..', 'package.json');
+
+//Remove "-beta" suffix
 const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, 'utf8'));
-
-// Modify the version to include the _beta suffix
-packageJson.version = `${packageJson.version}-beta`;
-
-// Write the updated package.json back to the file
+packageJson.version = packageJson.version.replace("-beta", "");
 fs.writeFileSync(packageJsonPath, JSON.stringify(packageJson, undefined, "	"));
 
-console.info(`Version bumped to ${packageJson.version}`);
+console.log("Update", upgradeType);
+execSync("npm version "+upgradeType+" --no-git-tag-version", { stdio: "inherit" });
+console.log("Update done", packageJson.version);
+
+//Add "-beta" suffix
+const packageJsonNew = JSON.parse(fs.readFileSync(packageJsonPath, 'utf8'));
+packageJsonNew.version = `${packageJsonNew.version}-beta`;
+fs.writeFileSync(packageJsonPath, JSON.stringify(packageJsonNew, undefined, "	"));
+
+console.info(`Version bumped to ${packageJsonNew.version}`);
+
+execSync("git add .", { stdio: "inherit" });
+const commitMessage = `Upgrade version to ${packageJsonNew.version}`;
+execSync(`git commit -m "${commitMessage}"`, { stdio: "inherit" });
+console.log(`Committed changes with message: "${commitMessage}"`);
