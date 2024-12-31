8b1921bd5b810c7df08b771cd27f4d41fb7b5b47
Durss <francois.dursus@gmail.com>
2024-12-31 18:29:27 +0100

    fix: updating chan IDs filter hashmap on tiktok/YouTube connected state change

--- src_front/components/messages/MessageList.vue
+++ src_front/components/messages/MessageList.vue
@@ -125,6 +125,7 @@ import MessageItem from './MessageItem.vue';
 import MessageListFilter, {MessageListFilter as MessageListFilterClass} from './components/MessageListFilter.vue';
 import { RoughEase } from 'gsap/all';
 import { Linear } from 'gsap/all';
+import YoutubeHelper from '@/utils/youtube/YoutubeHelper';
 
 @Component({
 	components: {
@@ -238,6 +239,8 @@ class MessageList extends Vue {
 		watch(()=>this.$store.params.features.mergeConsecutive_maxSizeTotal.value, async () => this.fullListRefresh());
 		watch(()=>this.$store.params.features.mergeConsecutive_minDuration.value, async () => this.fullListRefresh());
 		watch(()=>this.$store.stream.connectedTwitchChans, ()=> this.rebuildChannelIdsHashmap(), {deep:true});
+		watch(()=>this.$store.tiktok.connected, ()=> this.rebuildChannelIdsHashmap(), {deep:true});
+		watch(()=>YoutubeHelper.instance.connected, ()=> this.rebuildChannelIdsHashmap(), {deep:true});
 		watch(()=>this.config, ()=> this.rebuildChannelIdsHashmap(), {deep:true});
 
 		this.publicApiEventHandler = (e) => this.onPublicApiEvent(e);
@@ -433,12 +436,14 @@ class MessageList extends Vue {
 	 * channel source.
 	 */
 	private rebuildChannelIdsHashmap() {
+		//Check for all valid IDs depending on the connected platforms
 		const validIds = this.$store.stream.connectedTwitchChans.concat().map(v=>v.user.id);
 		if(this.$store.auth.youtube.user) validIds.push(this.$store.auth.youtube.user.id);
 		if(this.$store.tiktok.connected) validIds.push("tiktok");
 		validIds.push(this.$store.auth.twitch.user.id);
+		
+		//Only keep configured entries that match a valid channel ID
 		const chanIds:{[uid:string]:boolean} = {};
-
 		Object.keys(this.config.channelIDs || {})
 		.filter(id=>validIds.includes(id))
 		.forEach(id => {
