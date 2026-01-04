67b271621b2ebea110af6ce0f099bbe00889be13
Durss <francois.dursus@gmail.com>
2026-01-04 18:46:42 +0100

    feat: adding SD API secret key auth

--- src_back/utils/DataSchema.ts
+++ src_back/utils/DataSchema.ts
@@ -2710,6 +2710,7 @@ const UserDataSchema = {
 			additionalProperties: false,
 			properties: {
 				ip: {type:"string", maxLength:100},
+				secretKey: {type:"string", maxLength:50},
 			}
 		}
 	}
