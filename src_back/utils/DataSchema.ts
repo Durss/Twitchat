00ac8f5fe1bfaa20e31b64dbfe2f127811d055a2
Durss <francois.dursus@gmail.com>
2026-01-09 00:34:40 +0100

    feat(quiz): progressing on quiz creation form

--- src_back/utils/DataSchema.ts
+++ src_back/utils/DataSchema.ts
@@ -2744,15 +2744,17 @@ const UserDataSchema = {
 											type: "object",
 											additionalProperties: false,
 											properties: {
+												id: { type: "string", maxLength:50 },
 												question: { type: "string" },
 												duration_s: { type: "integer" },
 												loosePointsOnFail: { type: "number" },
-												answers: {
+												answerList: {
 													type: "array",
 													items: {
 														type: "object",
 														additionalProperties: false,
 														properties: {
+															id: { type: "string", maxLength:50 },
 															title: { type: "string" },
 															correct: { type: "boolean" }
 														}
@@ -2772,14 +2774,16 @@ const UserDataSchema = {
 											type: "object",
 											additionalProperties: false,
 											properties: {
+												id: { type: "string", maxLength:50 },
 												question: { type: "string" },
 												duration_s: { type: "integer" },
-												answers: {
+												answerList: {
 													type: "array",
 													items: {
 														type: "object",
 														additionalProperties: false,
 														properties: {
+															id: { type: "string", maxLength:50 },
 															title: { type: "string" }
 														}
 													}
