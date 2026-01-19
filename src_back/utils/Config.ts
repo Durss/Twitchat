b3aac4056829114f31f5d8177828626b0eef5150
Durss <francois.dursus@gmail.com>
2026-01-19 23:58:30 +0100

    fix(bingo): caching not properly updating + twitchext state
    There was a caching issue, it wasn't properly updating after changing the grid's config
    Adding a global "streamer state" endpoint for twitch extension to get any running bingo or quiz.
    Adding viewers notifications of configs updates

--- src_back/utils/Config.ts
+++ src_back/utils/Config.ts
@@ -523,6 +523,7 @@ interface Credentials {
 	twitchExtension_client_id: string;
 	twitchExtension_client_secret: string;
 	
+	twitchat_api_path: string;
 	twitchat_api_secret: string;
 
 	spotify_client_id: string;
