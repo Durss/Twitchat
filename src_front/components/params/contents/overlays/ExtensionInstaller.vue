90560db84210acacb818c1fc32012673913ab848
Durss <francois.dursus@gmail.com>
2026-01-20 22:13:47 +0100

    feat(bingoGrid): drop HEAT and CHAT commands features

---
+++ src_front/components/params/contents/overlays/ExtensionInstaller.vue
@@ -0,0 +1,33 @@
+<template>
+	<div class="extensioninstaller card-item">
+		test
+	</div>
+</template>
+
+<script lang="ts">
+import Config from '@/utils/Config';
+import {toNative,  Component, Vue } from 'vue-facing-decorator';
+
+@Component({
+	components:{},
+	emits:[],
+})
+class ExtensionInstaller extends Vue {
+	
+	mounted():void {
+		this.$store.extension.availableExtensions.findIndex(ext=> {
+			if(ext.id == Config.instance.TWITCH_EXTENSION_ID) {
+				console.log("FOUND IT", ext);
+			}
+		});
+	}
+
+}
+export default toNative(ExtensionInstaller);
+</script>
+
+<style scoped lang="less">
+.extensioninstaller{
+	
+}
+</style>
