d1515590981c51abcdd2d3bd53bb09736af47f96
Durss <francois.dursus@gmail.com>
2026-02-25 00:49:26 +0100

    feat(quiz) showing vote percents
    Fine tunning premium states

--- src_front/store/StoreProxy.ts
+++ src_front/store/StoreProxy.ts
@@ -4008,6 +4008,15 @@ export interface IQuizActions {
 	 * Broadcasts currently enabled quiz state to overlays
 	 */
 	broadcastQuizState(overlayOnly?:boolean):void;
+	/**
+	 * Computes global and relative percents for each answer of given quiz question based on current votes
+	 * Global percent is the actual percent of votes for this answer among all votes
+	 * Relative percent is based on the highest voted answer. Highest voted answer is 100%,
+	 * other answers percent are computed based on that.
+	 * @param quizId 
+	 * @param questionId 
+	 */
+	computeQuestionPercents(quizId:string, questionId:string): {[answerId: string]: {global:number, relative:number}}
 }
 
 
