5a3fe30affc1d1454ba2c17b28157bb390c77b59
Durss <francois.dursus@gmail.com>
2026-05-24 23:59:01 +0200

    refactor: composition API migration
    Showing snooze button above button to avoid reflow

--- src_front/components/chatform/CommercialTimer.vue
+++ src_front/components/chatform/CommercialTimer.vue
@@ -1,15 +1,16 @@
 <template>
-	<div :class="classes" v-if="showTimer">
+	<div :class="classes" v-if="showTimer" ref="rootEl">
 		<Icon name="ad" />
 
 		<div
 			class="timer"
 			ref="label"
-			v-tooltip="
-				isAdRunning
-					? $t('global.tooltips.commercial')
-					: $t('global.tooltips.commercial_soon')
-			"
+			v-tooltip="{
+				content: isAdRunning
+					? t('global.tooltips.commercial')
+					: t('global.tooltips.commercial_soon'),
+				offset: [0, 35],
+			}"
 		>
 			{{ timeLeftFormatted }}s
 		</div>
@@ -23,7 +24,7 @@
 			ref="snoozeBt"
 			class="snoozeBt"
 			@click.capture="snooze()"
-			v-tooltip="$t('global.tooltips.commercial_snooze')"
+			v-tooltip="t('global.tooltips.commercial_snooze')"
 			:loading="snoozing"
 			>Snooze {{ snoozeLeft }}/{{ snoozeMax }}</Button
 		>
@@ -39,117 +40,125 @@
 	</div>
 </template>
 
-<script lang="ts">
+<script setup lang="ts">
 import Utils from "@/utils/Utils";
-import { toNative, Component, Vue } from "vue-facing-decorator";
 import Icon from "../Icon.vue";
 import TTButton from "../TTButton.vue";
 import TwitchUtils from "@/utils/twitch/TwitchUtils";
 import { TwitchScopes } from "@/utils/twitch/TwitchScopes";
 import { gsap } from "gsap/gsap-core";
+import { computed, nextTick, onBeforeUnmount, onMounted, ref, useTemplateRef } from "vue";
+import { useI18n } from "vue-i18n";
+import { storeAuth as useStoreAuth } from "@/store/auth/storeAuth";
+import { storeStream as useStoreStream } from "@/store/stream/storeStream";
 
-@Component({
-	components: {
-		Icon,
-		Button: TTButton,
-	},
-})
-class CommercialTimer extends Vue {
-	public showTimer: boolean = false;
-	public error: boolean = false;
-	public canSnooze: boolean = false;
-	public snoozing: boolean = false;
-	public isAdComing: boolean = true;
-	public isAdRunning: boolean = true;
-	public timeLeft: number = 0;
-	public timeLeftFormatted: string = "";
-	public snoozeLeft: number = 3;
-	public snoozeMax: number = 3;
+const Button = TTButton;
 
-	private interval: number = -1;
+const { t } = useI18n();
+const storeAuth = useStoreAuth();
+const storeStream = useStoreStream();
 
-	public get classes(): string[] {
-		const res = ["commercialtimer"];
-		if (this.isAdRunning) res.push("secondary");
-		return res;
+const showTimer = ref<boolean>(false);
+const error = ref<boolean>(false);
+const canSnooze = ref<boolean>(false);
+const snoozing = ref<boolean>(false);
+const isAdComing = ref<boolean>(true);
+const isAdRunning = ref<boolean>(true);
+const timeLeft = ref<number>(0);
+const timeLeftFormatted = ref<string>("");
+const snoozeLeft = ref<number>(3);
+const snoozeMax = ref<number>(3);
+
+const rootEl = useTemplateRef<HTMLDivElement>("rootEl");
+const label = useTemplateRef<HTMLDivElement>("label");
+
+let interval: number = -1;
+
+const classes = computed<string[]>(() => {
+	const res = ["commercialtimer"];
+	if (isAdRunning.value) res.push("secondary");
+	return res;
+});
+
+function refreshTimer(): void {
+	const maxSchedule = 10 * 60000;
+	const channelId = storeAuth.twitch.user.id;
+	const infos = storeStream.getCommercialInfo(channelId);
+	snoozeLeft.value = infos.remainingSnooze;
+	snoozeMax.value = Math.max(3, infos.remainingSnooze); //Not 100% sure we get 3 snooze max so we get the max of both values
+	//Check if an ad is rolling
+	isAdComing.value = false;
+	isAdRunning.value = false;
+	let startDate: number = 0;
+	if (infos.prevAdStart_at + infos.currentAdDuration_ms >= Date.now()) {
+		isAdRunning.value = true;
+		startDate = infos.prevAdStart_at + infos.currentAdDuration_ms;
+	} else if (
+		Date.now() > infos.nextAdStart_at &&
+		Date.now() < infos.nextAdStart_at + infos.currentAdDuration_ms
+	) {
+		isAdRunning.value = true;
+		startDate = infos.nextAdStart_at + infos.currentAdDuration_ms;
+	} else //Check if an ad is coming in less than "maxSchedule"" minutes
+	 if (infos.nextAdStart_at > 0 && infos.nextAdStart_at - Date.now() < maxSchedule) {
+		isAdComing.value = true;
+		startDate = infos.nextAdStart_at;
 	}
+	timeLeft.value = Math.max(0, Math.round((startDate - Date.now()) / 1000));
+	timeLeftFormatted.value = Utils.formatDuration(timeLeft.value * 1000);
 
-	public mounted(): void {
-		this.refreshTimer();
-		this.interval = window.setInterval(() => this.refreshTimer(), 1000);
-		this.canSnooze = TwitchUtils.hasScopes([TwitchScopes.ADS_SNOOZE]);
-	}
-
-	public beforeUnmount(): void {
-		clearInterval(this.interval);
-	}
-
-	public refreshTimer(): void {
-		const maxSchedule = 10 * 60000;
-		const channelId = this.$store.auth.twitch.user.id;
-		const infos = this.$store.stream.getCommercialInfo(channelId);
-		this.snoozeLeft = infos.remainingSnooze;
-		this.snoozeMax = Math.max(3, infos.remainingSnooze); //Not 100% sure we get 3 snooze max so we get the max of both values
-		//Check if an ad is rolling
-		this.isAdComing = false;
-		this.isAdRunning = false;
-		let startDate: number = 0;
-		if (infos.prevAdStart_at + infos.currentAdDuration_ms >= Date.now()) {
-			this.isAdRunning = true;
-			startDate = infos.prevAdStart_at + infos.currentAdDuration_ms;
-		} else if (
-			Date.now() > infos.nextAdStart_at &&
-			Date.now() < infos.nextAdStart_at + infos.currentAdDuration_ms
-		) {
-			this.isAdRunning = true;
-			startDate = infos.nextAdStart_at + infos.currentAdDuration_ms;
-		} else //Check if an ad is coming in less than "maxSchedule"" minutes
-		 if (infos.nextAdStart_at > 0 && infos.nextAdStart_at - Date.now() < maxSchedule) {
-			this.isAdComing = true;
-			startDate = infos.nextAdStart_at;
-		}
-		this.timeLeft = Math.max(0, Math.round((startDate - Date.now()) / 1000));
-		this.timeLeftFormatted = Utils.formatDuration(this.timeLeft * 1000);
-
-		const prevShow = this.showTimer;
-		this.showTimer = (this.isAdRunning || this.isAdComing) && this.timeLeft > 0;
-		if (!prevShow && this.showTimer) {
-			this.$nextTick().then(() => {
-				this.openAnimation();
-			});
-		}
-	}
-
-	public openAnimation(): void {
-		// const snooze = (this.$refs.snoozeBt as ComponentPublicInstance).$el as HTMLDivElement | undefined;
-		// if(snooze) {
-		// 	gsap.from(snooze, {duration:.25, width:0, padding:0, clearProps:"all", delay: 1.5, ease:"sine.inOut"});
-		// }
-		const label = this.$refs.label as HTMLDivElement;
-		gsap.from(label, {
-			duration: 0.5,
-			width: 0,
-			padding: 0,
-			clearProps: "all",
-			delay: 0.5,
-			ease: "sine.inOut",
+	const prevShow = showTimer.value;
+	showTimer.value = (isAdRunning.value || isAdComing.value) && timeLeft.value > 0;
+	if (!prevShow && showTimer.value) {
+		nextTick().then(() => {
+			openAnimation();
 		});
-		gsap.from(this.$el, { duration: 0.5, width: 0, height: 0, padding: 0, ease: "sine.inOut" });
-		gsap.from(this.$el, { duration: 0.5, gap: 0, clearProps: "all", ease: "sine.inOut" });
-	}
-
-	public async snooze(): Promise<void> {
-		if (!this.canSnooze) {
-			TwitchUtils.requestScopes([TwitchScopes.ADS_SNOOZE]);
-		} else {
-			this.snoozing = true;
-			const res = await TwitchUtils.snoozeNextAd();
-			this.error = res == null;
-			this.snoozing = false;
-		}
 	}
 }
-export default toNative(CommercialTimer);
+
+function openAnimation(): void {
+	// const snooze = (snoozeBt.value as ComponentPublicInstance).$el as HTMLDivElement | undefined;
+	// if(snooze) {
+	// 	gsap.from(snooze, {duration:.25, width:0, padding:0, clearProps:"all", delay: 1.5, ease:"sine.inOut"});
+	// }
+	gsap.from(label.value!, {
+		duration: 0.5,
+		width: 0,
+		padding: 0,
+		clearProps: "all",
+		delay: 0.5,
+		ease: "sine.inOut",
+	});
+	gsap.from(rootEl.value!, {
+		duration: 0.5,
+		width: 0,
+		height: 0,
+		padding: 0,
+		ease: "sine.inOut",
+	});
+	gsap.from(rootEl.value!, { duration: 0.5, gap: 0, clearProps: "all", ease: "sine.inOut" });
+}
+
+async function snooze(): Promise<void> {
+	if (!canSnooze.value) {
+		TwitchUtils.requestScopes([TwitchScopes.ADS_SNOOZE]);
+	} else {
+		snoozing.value = true;
+		const res = await TwitchUtils.snoozeNextAd();
+		error.value = res == null;
+		snoozing.value = false;
+	}
+}
+
+onMounted(() => {
+	refreshTimer();
+	interval = window.setInterval(() => refreshTimer(), 1000);
+	canSnooze.value = TwitchUtils.hasScopes([TwitchScopes.ADS_SNOOZE]);
+});
+
+onBeforeUnmount(() => {
+	clearInterval(interval);
+});
 </script>
 
 <style scoped lang="less">
@@ -165,15 +174,11 @@ export default toNative(CommercialTimer);
 	// padding: .25em .5em;
 	padding: 0 0.5em;
 	border-radius: var(--border-radius);
-	overflow: hidden;
+	position: relative;
 
 	&.secondary {
 		background-color: var(--color-secondary);
 		text-shadow: 1px 1px 0 rgba(0, 0, 0, 0.5);
-	}
-
-	&:has(.button, .error) {
-		padding-right: 0;
 	}
 
 	.icon {
@@ -185,27 +190,38 @@ export default toNative(CommercialTimer);
 		cursor: default;
 	}
 	.snoozeBt {
-		max-width: 0;
-		padding: 0;
-		height: 100%;
+		position: absolute;
 		flex-wrap: nowrap;
-		transition: all 0.25s;
+		width: max-content;
+		bottom: 100%;
+		left: 50%;
+		transform: translate(-50%, 0) !important;
+		white-space: nowrap;
+		opacity: 0;
+		pointer-events: none;
+		transition: opacity 0.2s;
 		:deep(.label) {
 			white-space: nowrap;
-			text-overflow: clip;
 		}
 	}
 
 	&:hover {
 		.snoozeBt {
-			max-width: 300px;
-			padding: 0.3em 1em;
+			opacity: 1;
+			pointer-events: auto;
 		}
 	}
 
 	.error {
 		font-weight: bold;
 		cursor: pointer;
+		position: absolute;
+		flex-wrap: nowrap;
+		width: max-content;
+		bottom: 100%;
+		left: 50%;
+		transform: translate(-50%, 0) !important;
 	}
 }
 </style>
+
