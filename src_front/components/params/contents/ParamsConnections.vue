c5c1e73b13cd91d181fb223aeda95840ee9621f6
Durss <francois.dursus@gmail.com>
2026-06-21 18:55:19 +0200

    fix: removed typo "*" attribute crashing view on OBS

--- src_front/components/params/contents/ParamsConnections.vue
+++ src_front/components/params/contents/ParamsConnections.vue
@@ -34,7 +34,6 @@
 				:class="{ connected: storeApi.connected, disabled: !storeApi.connected }"
 				@click="subContent = 'twitchat_api'"
 				v-newflag="{ date: $config.NEW_FLAGS_DATE_V17, id: 'params_connect.twitchat_api' }"
-				*
 				key="twitchat_api"
 			>
 				<Icon name="twitchat_api" />
