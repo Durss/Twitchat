9b3f0fd767c337a3b225b9abf49ae29d3a027644
Durss <francois.dursus@gmail.com>
2026-08-08 19:45:22 +0200

    refactor: composition API migration

--- src_front/components/params/contents/triggers/entries/TriggerActionChatEntry.vue
+++ src_front/components/params/contents/triggers/entries/TriggerActionChatEntry.vue
@@ -6,25 +6,28 @@
 			forceChildDisplay
 			:error="cmdNameConflict"
 			:errorMessage="$t('triggers.actions.chat.loop')"
+			chatPreview
 		>
-			<ToggleBlock
-				class="commands"
-				:title="$t('triggers.actions.chat.commands_list')"
-				small
-				:open="false"
-			>
-				<div
-					class="cmd"
-					v-for="c in sortedCommands"
-					v-tooltip="$t('global.placeholder_selector_insert')"
-					@click="insertCommand(c)"
-					v-html="
-						c.cmd
-							.replace(/(\/\S+)/gi, '<mark>$1</mark>')
-							.replace(/(?:\{([^}]+)\}?)/gi, ' [$1]')
-					"
-				></div>
-			</ToggleBlock>
+			<template #bellow_placeholders>
+				<ToggleBlock
+					class="commands"
+					:title="$t('triggers.actions.chat.commands_list')"
+					small
+					:open="false"
+				>
+					<div
+						class="cmd"
+						v-for="c in sortedCommands"
+						v-tooltip="$t('global.placeholder_selector_insert')"
+						@click="insertCommand(c)"
+						v-html="
+							c.cmd
+								.replace(/(\/\S+)/gi, '<mark>$1</mark>')
+								.replace(/(?:\{([^}]+)\}?)/gi, ' [$1]')
+						"
+					></div>
+				</ToggleBlock>
+			</template>
 		</ParamItem>
 
 		<ParamItem
@@ -35,8 +38,10 @@
 	</div>
 </template>
 
-<script lang="ts">
+<script setup lang="ts">
 import ToggleBlock from "@/components/ToggleBlock.vue";
+import { useTriggerActionPlaceholders } from "@/composables/useTriggerActionPlaceholders";
+import { storeChat as useStoreChat } from "@/store/chat/storeChat";
 import {
 	TriggerTypes,
 	type ITriggerPlaceholder,
@@ -45,89 +50,81 @@ import {
 	type TriggerTypesValue,
 } from "@/types/TriggerActionDataTypes";
 import type { TwitchatDataTypes } from "@/types/TwitchatDataTypes";
-import { toNative, Component, Prop } from "vue-facing-decorator";
+import { computed, onBeforeMount, ref } from "vue";
 import ParamItem from "../../../ParamItem.vue";
-import AbstractTriggerActionEntry from "./AbstractTriggerActionEntry";
 
-@Component({
-	components: {
-		ParamItem,
-		ToggleBlock,
-	},
-	emits: ["update"],
-})
-class TriggerActionChatEntry extends AbstractTriggerActionEntry {
-	@Prop
-	declare action: TriggerActionChatData;
+const props = defineProps<{
+	action: TriggerActionChatData;
+	triggerData: TriggerData;
+}>();
 
-	@Prop
-	declare triggerData: TriggerData;
+const storeChat = useStoreChat();
 
-	public param_message: TwitchatDataTypes.ParameterData<string> = {
-		type: "string",
-		longText: true,
-		value: "",
-		icon: "whispers",
-		maxLength: 500,
-		labelKey: "triggers.actions.chat.param_message",
-	};
-	public param_reply: TwitchatDataTypes.ParameterData<boolean> = {
-		type: "boolean",
-		value: false,
-		icon: "reply",
-		labelKey: "triggers.actions.chat.param_reply",
-	};
+const param_message = ref<TwitchatDataTypes.ParameterData<string>>({
+	type: "string",
+	longText: true,
+	value: "",
+	icon: "whispers",
+	maxLength: 500,
+	labelKey: "triggers.actions.chat.param_message",
+});
+const param_reply = ref<TwitchatDataTypes.ParameterData<boolean>>({
+	type: "boolean",
+	value: false,
+	icon: "reply",
+	labelKey: "triggers.actions.chat.param_reply",
+});
 
-	public get cmdNameConflict(): boolean {
-		if (!this.action.text) return false;
-		const cmds = this.triggerData.chatCommandAliases?.concat() ?? [];
-		if (this.triggerData.chatCommand) cmds.push(this.triggerData.chatCommand);
-		const cmd = this.action.text.split(" ")[0]!.toLowerCase();
-		for (let i = 0; i < cmds.length; i++) {
-			if (cmds[i] == cmd) return true;
-		}
-		return false;
+const cmdNameConflict = computed<boolean>(() => {
+	if (!props.action.text) return false;
+	const cmds = props.triggerData.chatCommandAliases?.concat() ?? [];
+	if (props.triggerData.chatCommand) cmds.push(props.triggerData.chatCommand);
+	const cmd = props.action.text.split(" ")[0]!.toLowerCase();
+	for (let i = 0; i < cmds.length; i++) {
+		if (cmds[i] == cmd) return true;
 	}
+	return false;
+});
 
-	public get sortedCommands(): TwitchatDataTypes.CommandData[] {
-		let res = this.$store.chat.commands.filter((v) => v.twitchCmd);
-		res.sort((a, b) => {
-			if (a.cmd > b.cmd) return 1;
-			if (a.cmd < b.cmd) return -1;
-			return 0;
-		});
-		return res;
-	}
+const sortedCommands = computed<TwitchatDataTypes.CommandData[]>(() => {
+	let res = storeChat.commands.filter((v) => v.twitchCmd);
+	res.sort((a, b) => {
+		if (a.cmd > b.cmd) return 1;
+		if (a.cmd < b.cmd) return -1;
+		return 0;
+	});
+	return res;
+});
 
-	public get canReply(): boolean {
-		return (
-			[
-				TriggerTypes.CHAT_COMMAND,
-				TriggerTypes.ANY_MESSAGE,
-				TriggerTypes.FIRST_TODAY,
-				TriggerTypes.FIRST_ALL_TIME,
-				TriggerTypes.CHEER,
-			] as TriggerTypesValue[]
-		).includes(this.triggerData.type);
-	}
+const canReply = computed<boolean>(() => {
+	return (
+		[
+			TriggerTypes.CHAT_COMMAND,
+			TriggerTypes.ANY_MESSAGE,
+			TriggerTypes.FIRST_TODAY,
+			TriggerTypes.FIRST_ALL_TIME,
+			TriggerTypes.CHEER,
+		] as TriggerTypesValue[]
+	).includes(props.triggerData.type);
+});
 
-	public insertCommand(cmd: TwitchatDataTypes.CommandData): void {
-		this.param_message.value =
-			cmd.cmd.replace(/(?:\{([^}]+)\}?)/gi, "$1") + "" + this.param_message.value;
-	}
-
-	public beforeMount(): void {
-		if (!this.action.text) this.action.text = "";
-	}
-
-	/**
-	 * Called when the available placeholder list is updated
-	 */
-	public onPlaceholderUpdate(list: ITriggerPlaceholder<any>[]): void {
-		this.param_message.placeholderList = list;
-	}
+function insertCommand(cmd: TwitchatDataTypes.CommandData): void {
+	param_message.value.value =
+		cmd.cmd.replace(/(?:\{([^}]+)\}?)/gi, "$1") + "" + param_message.value.value;
 }
-export default toNative(TriggerActionChatEntry);
+
+onBeforeMount(() => {
+	if (!props.action.text) props.action.text = "";
+});
+
+/**
+ * Called when the available placeholder list is updated
+ */
+function onPlaceholderUpdate(list: ITriggerPlaceholder<any>[]): void {
+	param_message.value.placeholderList = list;
+}
+
+useTriggerActionPlaceholders(props.action, props.triggerData, onPlaceholderUpdate);
 </script>
 
 <style scoped lang="less">
