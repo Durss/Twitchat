87cb3c6210e7819820bdc9f3006481935c1893ef
Durss <francois.dursus@gmail.com>
2026-09-08 23:46:10 +0200

    feat: add helper to close toaster by its ID

--- src_front/utils/toast/toast.ts
+++ src_front/utils/toast/toast.ts
@@ -1,11 +1,17 @@
 import StoreProxy from "@/store/StoreProxy";
-import { toast as vToast, type Content, type ToastOptions } from "vue3-toastify";
+import { toast as vToast, type Content, type Id, type ToastOptions } from "vue3-toastify";
 
-export function toast(content: Content, options?: ToastOptions) {
-	vToast(content, {
+export function toast(content: Content, options?: ToastOptions): Id {
+	return vToast(content, {
 		theme: StoreProxy.common.theme == "dark" ? "dark" : "light",
 		...options,
 	});
+}
 
-	return {};
+/**
+ * Closes a toast opened with toast(), useful for the ones opened with
+ * autoClose:false
+ */
+export function closeToast(id: Id): void {
+	vToast.remove(id);
 }
